2ee5353e72baa03fba6dc1cdc54b7b414476ffef
David Berezowski <david.berezowski1@gmail.com>
2024-12-02 18:30:55 +0100

    Fully implemented (for now) Duration class

--- data/misc/Duration.ts
+++ data/misc/Duration.ts
@@ -1,10 +1,27 @@
 
-
 export class Duration {
     private durationInMinutes: number;
 
-    public constructor(durationInMinutes: number) {
+    private constructor(durationInMinutes: number) {
         this.durationInMinutes = durationInMinutes
     }
 
+    public static ofMinutes(minutes: number) {
+        return new Duration(minutes);
+    }
+
+    public static ofHoursAndMinutes(hours: number, minutes: number) {
+        return new Duration(hours * 60 + minutes);
+    }
+
+    public toString() {
+        const hours = Math.floor(this.durationInMinutes / 60);
+        const minutes = this.durationInMinutes % 60;
+
+        if (hours == 0 && minutes == 0) {
+            return "0min";
+        }
+
+        return [(hours != 0 ? `${hours}h` : ''), (minutes != 0 ? `${minutes}min` : '')].join(" ");
+    }
 }
